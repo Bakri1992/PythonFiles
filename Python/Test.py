84ffc7029003d1ebb580b491b068a7d4bb5c0d7f
Bakri <bakri.badet1992@gmail.com>
2023-11-19 15:36:09 +0100

    update

--- Python/Test.py
+++ Python/Test.py
@@ -1,10 +1,1 @@
-import copy
-languages={
-    "one"  :{"name":"C++","progress":"50%"},
-    "two"  :{"name":"Python","progress":"80%"},
-    "three":{"name":"R","progress":"30%"}
-}
-cplan =copy.deepcopy(languages)
-
-print(id(languages["one"]["name"]))
-print(id(cplan["one"]["name"]))
+")
